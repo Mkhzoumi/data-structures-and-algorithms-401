ce92ca0f5cfa9a6ffc036cad90da09186edafbf8
mkhzoumi <zx.hammam@hotmail.com>
2021-07-28 01:05:15 +0300

    odifying

--- array-binary-search/README.md
+++ array-binary-search/README.md
@@ -3,4 +3,4 @@
 ## find a specific value in a sorted array, using a binary search algorithm .
 
 ## Whiteboard Process
-![BinarySearch](BinarySearch.jpg)
+![BinarySearch](array-binary-search.jpg)
--- array-insert-shift/Trys.java
+++ array-insert-shift/Trys.java
@@ -1,10 +1,11 @@
+
 import java.util.Arrays;
 
 public class Trys {
     public static void main(String[] args) {
-        
-        int n=3;
-        int[] arr={1,2,4,5};
+
+        int n=4;
+        int[] arr={1,2,3,5,6};
         System.out.println(Arrays.toString(insertShiftArray(arr , n)));
     }
 
@@ -13,9 +14,9 @@ public class Trys {
     public static int[] insertShiftArray(int[] arr , int num){
         int[] newArr = new int[(arr.length+1)];
         for (int i = 0; i <= arr.length; i++) {
-            if (i < arr.length/2){
+            if (i < Math.ceil((double)arr.length/2)){
                 newArr[i]= arr[i];
-            }else if(i == arr.length/2){
+            }else if(i == Math.ceil((double)arr.length/2)){
                 newArr[i] = num;
             }else{
                 newArr[i] = arr[i-1];
