3cd400a421b03760e8243fda65ab20b94e71e5db
mkhzoumi <zx.hammam@hotmail.com>
2021-09-01 21:49:08 +0300

    quick sort

--- Insertion-Sort/app/src/main/java/Insertion/Sort/App.java
+++ Insertion-Sort/app/src/main/java/Insertion/Sort/App.java
@@ -14,9 +14,11 @@ public class App {
     public static void main(String[] args) {
 
         int[] testing = {8,4,23,42,16,15};
-        System.out.println(Arrays.toString(mergeSort(testing)));
+//        System.out.println(Arrays.toString(mergeSort(testing)));
 
    //        System.out.println(Arrays.toString(insertionSort(testing)));
+
+        System.out.println(Arrays.toString(quickSort(testing, 0, testing.length-1)));
 }
 
     public static int[] insertionSort(int[] list){
@@ -32,7 +34,6 @@ public class App {
         }
         return list;
     }
-
 
 
     public static int[] mergeSort(int[] arr){
@@ -83,4 +84,42 @@ public class App {
         }
         return arr;
     }
+
+
+
+
+
+
+
+    public static int[] quickSort(int[] arr, int left, int right) {
+        if(left < right) {
+            int position = partition(arr, left, right);
+            quickSort(arr, left, position - 1);
+            quickSort(arr, position + 1, right);
+        }
+        return arr;
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int low = left - 1;
+        for(int i = left; i < right; i++) {
+            if(arr[i] < pivot) {
+                low++;
+                swap(arr, i, low);
+            }
+        }
+
+        swap(arr, right, low + 1);
+        return low + 1;
+    }
+
+    public static void swap(int[] arr, int i, int low) {
+        int temp;
+        temp = arr[i];
+        arr[i] = arr[low];
+        arr[low] = temp;
+    }
+
+
 }
