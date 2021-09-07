2bf8a3cb5f8e85cd19e72091f1939565451c6ec4
mkhzoumi <zx.hammam@hotmail.com>
2021-09-07 21:44:24 +0300

    challenge 32

--- hashmap-repeated-word/app/src/main/java/hashmap/repeated/word/App.java
+++ hashmap-repeated-word/app/src/main/java/hashmap/repeated/word/App.java
@@ -15,7 +15,7 @@ public class App {
 
     public static void main(String[] args) {
 //        System.out.println(new App().getGreeting());
-/
+
         String words = "It was the best of times, it was the worst of times,";
         System.out.println(repeatedWord(words));
     }
